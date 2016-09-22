db986a0b310d3a24504af8ad04871fd8859c856a
Alison Keen <ali.keen@gmail.com>
2016-09-22 05:12:57 +0000

    Skeleton scraper, need to go pick up child...

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+The beginnings of a Mechanize scraper to read Hansard dates
+
+Source page may need to change, as the official page is generated using javascript.
+
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,22 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
+require 'scraperwiki'
+require 'mechanize'
+
+calendar_search_url = "http://hansardpublic.parliament.sa.gov.au/#/search/1"
+
+
+agent = Mechanize.new
+
+# Read in a page
+page = agent.get(calendar_search_url)
+
+# Find somehing on the page using css selectors
+puts page.search('div.scheduler')
+
+puts "The problem here is that each month in the calendar has the dates populated dynamically using JavaScript on load."
+puts "I don't know how to scrape dynamically loaded pages, yet..." 
+
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
