1a096007ffe51151bea28af920daee4a370d13ae
Alison Keen <ali.keen@gmail.com>
2016-10-28 23:23:48 +0000

    Changed to use PhantomJS/Capybara/Poltergeist

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 The beginnings of a Mechanize scraper to read Hansard dates
 
-Source page may need to change, as the official page is generated using javascript.
+Scraper is built using Poltergeist / Capybara due to heavy AJAX insertion of content that doesn't appear when scraped with Mechanize
+
+Notes to self: 
+When debugging/running scraper on own ubuntu system, prerequisites are 
+[code]
+sudo gem install poltergeist
+[/code]
+Then go to the PhantomJS website and manually wget the PhantomJS build .tar.bz2 because the Ubuntu package is built against the wrong graphics toolkit and crashes on load. 
 
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,32 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#
+#  Scraper to read sitting dates for SA Parliament 
+# 
+# In order to facilitate automatically downloading each day's hansard
+# without having to manually download and save each day
+# 
+# This version of code hacked together by Alison Keen Oct/Nov 2016
+# 
+# Initial starting point blatantly plagiarized from 
+# 
+# https://github.com/openaustralia/example_ruby_phantomjs_scraper/blob/master/scraper.rb
+# 
+# (Thankyou OpenAustralia people, that's a really helpful example!!)
+#
 
 require 'scraperwiki'
-require 'mechanize'
+require 'capybara/poltergeist'
 
 calendar_search_url = "http://hansardpublic.parliament.sa.gov.au/#/search/1"
 
 
-agent = Mechanize.new
+capybara = Capybara::Session.new(:poltergeist)
+
 
 # Read in a page
-page = agent.get(calendar_search_url)
+capybara.visit(calendar_search_url)
 
 # Find somehing on the page using css selectors
-puts page.search('div.scheduler')
-
-puts "The problem here is that each month in the calendar has the dates populated dynamically using JavaScript on load."
-puts "I don't know how to scrape dynamically loaded pages, yet..." 
+puts capybara.find('div.scheduler').text
 
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
